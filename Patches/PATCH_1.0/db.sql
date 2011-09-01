6d56422fa06bc8f4a4752034c51c6ca689347171
Pedro Pires <zasdqw@gmail.com>
2011-09-01 16:49:24 +0100

    Bug Fix

--- Patches/PATCH_1.0/db.sql
+++ Patches/PATCH_1.0/db.sql
@@ -1,4 +1,4 @@
-UPDATE configParams SET configParams_value='1.5.2' WHERE configParams_id='AgendoVersion';
+UPDATE configParams SET configParams_value='1.5.2' WHERE configParams_name='AgendoVersion';
 INSERT INTO `configparams` (`configParams_name`, `configParams_value`) VALUES
 ('imapCheck', '0'),
 ('imapHost', ''),
