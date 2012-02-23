a817a985878ccb1f6eb3a84e1e96f5ba59367b8e
Pedro Pires <zasdqw@gmail.com>
2012-02-23 17:35:53 +0000

    Changed the hourUsage feature, now with a decent look, almost done

--- agendo/hoursUsage.php
+++ agendo/hoursUsage.php
@@ -8,117 +8,13 @@
 	if(
 		isset($_POST['userCheck'])
 		&& isset($_POST['resourceCheck'])
+		&& isset($_POST['entryCheck'])
 		&& isset($_POST['beginDate'])
 		&& isset($_POST['endDate'])
 	){
 		try{
-			$showSubTotal = false;
-			$colspan = 3;
-			$json->tableData = "";
-			$whereIsResp = "";
-			$whereDepartment = "user_id = entry_user and department_id = user_dep";
-			if(!$isAdmin){
-				if($isPI == false){
-					$sql = "select user_dep from ".dbHelp::getSchemaName().".user where user_id = :0";
-					$prep = dbHelp::query($sql, array($_SESSION['user_id']));
-					$row = dbHelp::fetchRowByIndex($prep);
-					$departments = $row[0];
-					$whereDepartment = "user_id = entry_user and user_dep = ".$departments." and department_id = user_dep";
-				}
-				else{
-					$departments = implode(",", $isPI);
-					$whereDepartment = "user_id = entry_user and user_dep in (".$departments.") and department_id = user_dep";
-				}
-				
-				if($isResp != false){
-					$whereIsResp = "or user_id = entry_user and user_dep not in (".$departments.") and department_id = user_dep and entry_resource in (".implode(",", $isResp).")";
-				}
-			}
-
-			$json->tableData .= "<tr style='border-bottom: 1px solid black;'>";
-				$json->tableData .= "<td>";
-					$json->tableData .= "Department";
-				$json->tableData .= "</td>";
-			
-				$entrySelect = "
-					sum(entry_slots * resource_resolution / 60) AS invoice_hours
-					,sum(entry_slots * resource_resolution * price_value / 60) AS invoice_price
-					,department_name AS invoice_department
-				";
-				$entryGroupBy = "";
-				if($_POST['entryCheck'] == 1){
-					$entrySelect = "
-						entry_slots * resource_resolution / 60 AS invoice_hours
-						,entry_slots * resource_resolution * price_value / 60 AS invoice_price
-						,department_name AS invoice_department
-						,entry_datetime
-					";
-					$entryGroupBy = ", entry_id order by department_name, entry_datetime";
-					
-					$json->tableData .= "<td>";
-						$json->tableData .= "Entry date";
-					$json->tableData .= "</td>";
-					$colspan++;
-				}
-				
-				$userSelect = "";
-				$userGroupBy = "";
-				if($_POST['userCheck'] == 1){
-					$userSelect = ",user_firstname, user_lastname";
-					$userGroupBy = ",user_firstname, user_lastname";
-					$showSubTotal = true;
-					
-					$json->tableData .= "<td colspan='2'>";
-						$json->tableData .= "User name";
-					$json->tableData .= "</td>";
-					
-					$colspan += 2;
-				}
-				
-				$resourceSelect = "";
-				$resourceGroupBy = "";
-				if($_POST['resourceCheck'] == 1){
-					$resourceSelect = ", resource_name, price_value";
-					$resourceGroupBy = ", resource_name";
-					$showSubTotal = true;
-					
-					$json->tableData .= "<td>";
-						$json->tableData .= "Resource";
-					$json->tableData .= "</td>";
-					
-					$json->tableData .= "<td>";
-						$json->tableData .= "Price per hour";
-					$json->tableData .= "</td>";
-					
-					$colspan += 2;
-				}
-			
-				$json->tableData .= "<td>";
-					$json->tableData .= "Usage time(hours)";
-				$json->tableData .= "</td>";
-				
-				$json->tableData .= "<td>";
-					$json->tableData .= "Cost";
-				$json->tableData .= "</td>";
-			$json->tableData .= "</tr>";
-			
-			$json->tableData .= "<tr>";
-				$json->tableData .= "<td colspan='".$colspan."'>";
-					$json->tableData .= "<hr>";
-				$json->tableData .= "</td>";
-			$json->tableData .= "</tr>";
-
-			$beginDate = dbHelp::convertToDate(str_replace("/", "-", $_POST['beginDate']));
-			$endDate = dbHelp::convertToDate(str_replace("/", "-", $_POST['endDate']));
-			
-			$json->tableData .= generateResults();
-
-			$json->tableData .= "<tr>";
-				$json->tableData .= "<td colspan='".$colspan."'>";
-					$json->tableData .= "<hr>";
-					$json->tableData .= "Total: ".$total;
-				$json->tableData .= "</td>";
-			$json->tableData .= "</tr>";
+			$json->tableData = generateResults($_POST['userCheck'], $_POST['resourceCheck'], $_POST['entryCheck'], $_POST['beginDate'], $_POST['endDate'], $isResp, $isAdmin, $isPI);
+			$json->tableData .= "<br>";
 			$json->success = true;
 		}
 		catch(Exception $e){
@@ -128,111 +24,212 @@
 		echo json_encode($json);
 		exit;
 	}
+	elseif(
+		isset($_POST['emailManagers'])
+		&& isset($_POST['departments'])
+	){
+		
+	}
 	
-	echo "<script type='text/javascript' src='js/jquery-1.5.2.min.js'></script>";
+	// echo "<script type='text/javascript' src='js/jquery-1.5.2.min.js'></script>";
+	importJs();
 	echo "<link href='css/jquery.datepick.css' rel='stylesheet' type='text/css' />";
+	echo "<link href='../agendo/css/hourUsage.css' rel='stylesheet' type='text/css' />";
 	echo "<script type='text/javascript' src='js/jquery.datepick.js'></script>";
 	echo "<script type='text/javascript' src='js/hoursUsage.js'></script>";
 	
-	echo "<table style='margin:auto;width:320px;text-align:right;'>";
+	echo "<br>";
+	echo "<h1>Resource Usage</h1>";
+	echo "<br>";
+	
+	echo "<table style='margin:auto;width:450px;text-align:right;'>";
 		echo "<tr>";
-			echo "<td>";
+			echo "<td style='text-align:left;'>";
 				echo "<a>From date:</a>";
-			echo "</td>";
-
-			echo "<td style='text-align:center;'>";
+				echo "&nbsp";
 				echo "<input type='text' id='beginDateText' style='text-align:center;'/>";
 			echo "</td>";
 
-			echo "<td>";
+			echo "<td style='text-align:right;'>";
+				echo "<a>To date:</a>";
+				echo "&nbsp";
+				echo "<input type='text' id='endDateText' style='text-align:center;'/>";
+			echo "</td>";
+		echo "</tr>";
+
+		echo "<tr>";
+			echo "<td style='text-align:center;'>";
+				echo "<label style='float:left;'>User";
+					echo "<input type='checkBox' id='userCheck'/>";
+				echo "</label>";
+
 				echo "<label>Resource";
 					echo "<input type='checkBox' id='resourceCheck'/>";
 				echo "</label>";
-			echo "</td>";
-		echo "</tr>";
 
-		echo "<tr>";
-			echo "<td>";
-				echo "<a>To date:</a>";
-			echo "</td>";
-
-			echo "<td style='text-align:center;'>";
-				echo "<input type='text' id='endDateText' style='text-align:center;'/>";
-			echo "</td>";
-
-			echo "<td>";
-				echo "<label>User";
-					echo "<input type='checkBox' id='userCheck'/>";
-				echo "</label>";
-			echo "</td>";
-		echo "</tr>";
-		
-		echo "<tr>";
-			echo "<td></td>";
-			
-			echo "<td style='text-align:center;'>";
-				echo "<input type='button' id='searchButton' value='Search' onclick='sendChecksAndGetResult()'/>";
-			echo "</td>";
-			
-			echo "<td>";
-				echo "<label>Entry";
+				echo "<label style='float:right;'>Entry";
 					echo "<input type='checkBox' id='entryCheck'/>";
 				echo "</label>";
 			echo "</td>";
+			
+			echo "<td style='text-align:right;'>";
+				echo "<input type='button' id='searchButton' value='Search' onclick='sendChecksAndGetResult()'/>";
+			echo "</td>";
 		echo "</tr>";
-	echo "</table>";
-	
-	// Table where the results will appear
-	echo "<table id='resultsTable' style='margin:auto;border:1px solid black;width:800;text-align:center;'>";
 	echo "</table>";
 	
 	echo "<br>";
 	
-	echo "<div style='margin:auto;width:200px;text-align:center;'>";
-		echo "<a style='color:black;text-align:center;' onmouseover=\"this.style.color='#F7C439'\" onmouseout=\"this.style.color='black'\" href='".$_SESSION['path']."/'>Back to reservations</a>";
+	// Table where the results will appear
+	echo "<div id='resultsTable' style='margin:auto;width:800;text-align:center;'>";
 	echo "</div>";
 	
-	// Returns a string with what the subtotal line should look for each department
-	function showSubTotal($departmentName, $subTotal, $colspan, $isAdmin){
-		$subColspan = 0;
-		$extraHtml = "";
-		if($isAdmin){
-			$subColspan = 2;
-			// Put the checkbox in a class for easy multi select and un-select
-			$extraHtml = 
-				"<td colspan='".$subColspan."' style='text-align:right;'>
-					<label>Mail the department manager
-						<input type='checkBox' id='department-".$departmentName."-Email'/>
-					</label>
-				</td>"
-			;
-		}
+	echo "<br>";
+
+	echo "<div style='margin:auto;width:200px;text-align:center;'>";
+		echo "<a class='link' href='".$_SESSION['path']."/'>Back to reservations</a>";
+	echo "</div>";
+	echo "<br>";
 	
-		$formatedString = "<tr>";
-			$formatedString .= "<td colspan='".($colspan-$subColspan)."'>";
-				$formatedString .= "Total for department '".$departmentName."': ".$subTotal;
-			$formatedString .= "</td>";
+	// Returns a string with what the subtotal line should look for each department
+	function showSubTotal($departmentName, $subTotal, $colspan, $isAdmin, $isPI, $showSubTotal){
+		$formatedString .= "<tr class='rowSubTotals'>";
+			$subColspan = $colspan;
+			if($isAdmin || $isPI != false){
+				$formatedString .= "<td>";
+					$formatedString .=  "<label title='Select to email the department manager'>Email";
+						$formatedString .=  "<input type='checkBox' class='emailChecks' id='".$departmentName."-EmailCheck' value='".$departmentName."'/>";
+					$formatedString .=  "</label>";
+				$formatedString .= "</td>";
+				$subColspan--;
+			}
 			
-			$formatedString .= $extraHtml;
+			if($showSubTotal){
+				$formatedString .= "<td colspan='".$subColspan."'>";
+					$formatedString .= "Total for department ".$departmentName.": <a id='".$departmentName."SubTotal' name='".$subTotal."'>".$subTotal."</a>";
+				$formatedString .= "</td>";
+				$subTotal = 0;
+			}
 		$formatedString .= "</tr>";
+		$formatedString .= "</table>";
+		$formatedString .= "<br>";
 		
 		return $formatedString;
 	}
 	
-	function generateResults(){
-		global 	$total, $colspan, $beginDate, $endDate, $showSubTotal, $isAdmin, $entrySelect, $userSelect, 
-				$resourceSelect, $whereDepartment, $resourceGroupBy, $userGroupBy, $entryGroupBy, $whereIsResp;
-		
+	// "Opens" a table (<table>) and adds the subHeader, subTotal function "closes" the table
+	function startTable($department, $subHeader, $colspan){
+		$formatedString = "<table id='".$department."Table' style='width:100%;text-align: center;'>";
+		$formatedString .= " 
+			<td class='rowNames' style='text-align:center;' colspan='".$colspan."'>
+				Department: ".$department."
+			</td>
+		";
+		return $formatedString.$subHeader;
+	}
+	
+	function generateResults($userCheck, $resourceCheck, $entryCheck, $beginDate, $endDate, $isResp, $isAdmin, $isPI){
+		$showSubTotal = false;
+		$colspan = 2;
 		$formatedString = "";
+		
+		$whereIsResp = "";
+		$whereDepartment = "user_id = entry_user and department_id = user_dep";
+		if(!$isAdmin){
+			if($isPI == false){
+				$sql = "select user_dep from ".dbHelp::getSchemaName().".user where user_id = :0";
+				$prep = dbHelp::query($sql, array($_SESSION['user_id']));
+				$row = dbHelp::fetchRowByIndex($prep);
+				$departments = $row[0];
+				$whereDepartment = "user_id = entry_user and user_dep = ".$departments." and department_id = user_dep";
+			}
+			else{
+				$departments = implode(",", $isPI);
+				$whereDepartment = "user_id = entry_user and user_dep in (".$departments.") and department_id = user_dep";
+			}
+			
+			if($isResp != false){
+				$whereIsResp = "or user_id = entry_user and user_dep not in (".$departments.") and department_id = user_dep and entry_resource in (".implode(",", $isResp).")";
+			}
+		}
+
+		$subHeader .= "<tr class='rowNames'>";
+				// sum(entry_slots * resource_resolution / 60) AS invoice_hours
+			$entrySelect = "
+				sum(entry_slots * resource_resolution) AS invoice_hours
+				,sum(entry_slots * resource_resolution * price_value / 60) AS invoice_price
+				,department_name AS invoice_department
+			";
+			$entryGroupBy = "";
+			if($_POST['entryCheck'] == 1){
+					// entry_slots * resource_resolution / 60 AS invoice_hours
+				$entrySelect = "
+					entry_slots * resource_resolution AS invoice_hours
+					,entry_slots * resource_resolution * price_value / 60 AS invoice_price
+					,department_name AS invoice_department
+					,entry_datetime
+				";
+				$entryGroupBy = ", entry_id order by department_name, entry_datetime";
+				
+				$subHeader .= "<td>";
+					$subHeader .= "Entry date";
+				$subHeader .= "</td>";
+				$colspan++;
+			}
+			
+			$userSelect = "";
+			$userGroupBy = "";
+			if($_POST['userCheck'] == 1){
+				$userSelect = ",user_firstname, user_lastname";
+				$userGroupBy = ",user_firstname, user_lastname";
+				$showSubTotal = true;
+				
+				$subHeader .= "<td colspan='2'>";
+					$subHeader .= "User name";
+				$subHeader .= "</td>";
+				
+				$colspan += 2;
+			}
+			
+			$resourceSelect = "";
+			$resourceGroupBy = "";
+			if($_POST['resourceCheck'] == 1){
+				$resourceSelect = ", resource_name, price_value";
+				$resourceGroupBy = ", resource_name";
+				$showSubTotal = true;
+				
+				$subHeader .= "<td>";
+					$subHeader .= "Resource";
+				$subHeader .= "</td>";
+				
+				$subHeader .= "<td>";
+					$subHeader .= "Price per hour";
+				$subHeader .= "</td>";
+				
+				$colspan += 2;
+			}
+		
+			$subHeader .= "<td>";
+				$subHeader .= "Usage time";
+			$subHeader .= "</td>";
+			
+			$subHeader .= "<td>";
+				$subHeader .= "Cost";
+			$subHeader .= "</td>";
+		$subHeader .= "</tr>";
+		
+		$beginDate = dbHelp::convertToDate(str_replace("/", "-", $_POST['beginDate']));
+		$endDate = dbHelp::convertToDate(str_replace("/", "-", $_POST['endDate']));
+			
 		$previousDepartmentName = "";
 		$subTotal = 0;
 		$staticWhere = "
 			and institute_id = department_inst
 			and price_type = institute_pricetype
 			and price_resource = entry_resource
-			and entry_status in (1,5)
+			and entry_status = 1
 			and resource_id = entry_resource
-			and entry_datetime between :0 and :1
+			and entry_datetime between '".$beginDate."' and '".$endDate."'
 		";
 
 		$resourcesManagerWhere = "";
@@ -262,24 +259,28 @@
 				".$userGroupBy."
 				".$entryGroupBy."
 		";
-
 		$prep = dbHelp::query($sql, array($beginDate, $endDate));
 		while($row = dbHelp::fetchRowByIndex($prep)){
 			$department = $row[2];
 			if(
 				$previousDepartmentName != $department
-				&& $showSubTotal
-				&& $previousDepartmentName != ""
 			){
-				$formatedString .= showSubTotal($previousDepartmentName, $subTotal, $colspan, $isAdmin);
-				$subTotal = 0;
+				if($previousDepartmentName != ""
+				){
+					$formatedString .= showSubTotal($previousDepartmentName, $subTotal, $colspan, $isAdmin, $isPI, ($showSubTotal || dbHelp::numberOfRows($prep) > 1));
+					$subTotal = 0;
+				}
+				$formatedString .= startTable($department, $subHeader, $colspan);
 			}
-			$hours = round($row[0], 2);
+
+			// $hours = round($row[2], 2);
+			$hours = floor($row[0] / 60);
+			$minutes = $row[0] % 60;
 			$priceTimesHours = round($row[1], 2);
 			
-			$formatedString .= "<tr>";
-				// Department and Names if User is checked
-				for($i=2; $i<sizeOf($row); $i++){
+			$formatedString .= "<tr class='resultsData'>";
+				// Names if User is checked
+				for($i=3; $i<sizeOf($row); $i++){
 					$formatedString .= "<td>";
 						$formatedString .= $row[$i];
 					$formatedString .= "</td>";
@@ -287,7 +288,8 @@
 				
 				// Hours
 				$formatedString .= "<td>";
-					$formatedString .= $hours;
+					// $formatedString .= $hours;
+					$formatedString .= $hours."h : ".$minutes."m";
 				$formatedString .= "</td>";
 				
 				// Price*Hours
@@ -302,10 +304,26 @@
 		}
 		
 		// Used to show the last subtotal
-		if($showSubTotal){
-			$formatedString .= showSubTotal($previousDepartmentName, $subTotal, $colspan, $isAdmin);
+		$formatedString .= showSubTotal($previousDepartmentName, $subTotal, $colspan, $isAdmin, $isPI, ($showSubTotal || dbHelp::numberOfRows($prep) > 1));
+		
+		$formatedString .= "<div class='rowTotal'>";
+			$formatedString .= "Total: ".$total;
+		$formatedString .= "</div>";
+		
+		if($isAdmin || $isPI != false){
+			$formatedString .=  "<div style='text-align:center;' colspan='".$colspan."'>";
+				$formatedString .=  "<label style='float:left;'>Select all email addresses";
+					$formatedString .=  "<input type='checkBox' id='allEmailsCheck' onclick='selectUnselectAll();'/>";
+				$formatedString .=  "</label>";
+
+				$formatedString .=  "<input style='float:right;' type='button' id='emailButton' value='Email Departments' onclick='email();'/>";
+			$formatedString .=  "</div>";
 		}
 		
 		return $formatedString;
+	}
+	
+	function emailManagers(){
+		
 	}
 ?>
--- agendo/js/hoursUsage.js
+++ agendo/js/hoursUsage.js
@@ -34,3 +34,47 @@ function sendChecksAndGetResult(){
 		alert('Please pick both dates');
 	}
 }
+
+// Selects/unselects all email check boxes
+function selectUnselectAll(){
+	var selectAllChecked = document.getElementById('allEmailsCheck').checked;
+	var elemArray = document.getElementById('resultsTable').getElementsByClassName('emailChecks');
+	
+	for(i in elemArray){
+		if(elemArray[i].id != null && elemArray[i].value){ // fix for chrome
+			elemArray[i].checked = selectAllChecked;
+		}
+	}
+}
+
+function email(){
+	var departmentsArray = new Array();
+	var elemArray = document.getElementById('resultsTable').getElementsByClassName('emailChecks');
+	
+	// Gets the selected departments to email
+	for(i in elemArray){
+		if(elemArray[i].id != null && elemArray[i].value && elemArray[i].checked){ // fix for chrome
+			departmentsArray.push(elemArray[i].value);
+		}
+	}
+
+	var subTotal;
+	// Iterates through the several departments (the ones checked)
+	for(i in departmentsArray){
+		subTotal = document.getElementById(departmentsArray[i] + 'SubTotal').name;
+		alert(subTotal);
+		// alert(departmentsArray[i]);
+	}
+}
+
+// Return an array with all the information regarding a department
+function getDepartmentData(department){
+	var departmentData = new Array();
+	var linesAndRows = new Array();
+	
+	
+	var subtotal = document.getElementById(department + 'SubTotal').name;
+	departmentData['subTotal'] = subTotal;
+	
+	return departmentData;
+}
