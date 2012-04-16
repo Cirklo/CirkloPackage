f5cd32216a24b8e1bec1968afe59f617e88d88da
Pedro Pires <zasdqw@gmail.com>
2012-04-16 10:53:19 +0100

    Bug fix with secureResources

--- agendo/js/sample.js
+++ agendo/js/sample.js
@@ -1,50 +1,24 @@
-function sampleInsert(resource){
-	var sampleName = document.getElementById('sampleName').value;
-	if(sampleName == ''){
-		showMessage('Please insert a sample name');
-	}
-	else{
-		$.post(
-			"../agendo/sampleHandling.php?resource=" + resource
-			,{sampleName: sampleName}
-			,function(serverData){
-				alert(serverData);
-			}
-			,"json")
-			.error(
-				function(errorData){
-					showMessage(errorData.responseText, true);
-				}
-			)
-		;
-	}
-}
-
+var userLogin = "";
+var userPass = "";
 // Sends the user login and pass from the input boxes if they are filled
 // if user is valid shows the sample insert div
 function sampleInsertShowDivAndCheckUser(resource){
-	// Nedeed because some idiot (me) put the same id for the field in weekview.php and commonCode.php (was it needed though?)
+	// Needed because some idiot (me) put the same id for the field in weekview.php and commonCode.php (maybe it was necessary and i cant remember... i hope...)
 	var objForm = document.getElementById('entrymanage');
-	var userLogin = objForm.user_id.value;
-	var userPass = objForm.user_passwd.value;
+	userLogin = objForm.user_id.value;
+	userPass = objForm.user_passwd.value;
 	objForm.user_id.value = "";
 	objForm.user_passwd.value = "";
 	// var userPass = document.getElementById('user_passwd').value;
 	$.post(
 		"../agendo/sampleHandling.php"
-		,{action:"sampleInsertHtml", userLogin:userLogin, userPass:userPass, resource:resource}
+		,{action: "sampleInsertHtml", userLogin: userLogin, userPass: userPass, resource: resource}
 		,function(serverData){
 			if(serverData.success){
-				// var divToShow = document.getElementById('sampleInterfaceDiv');
 				var divToShow = $('#sampleInterfaceDiv');
-				// divToShow.style.display = 'table';
-				divToShow.css("top", (($(window).height() - divToShow.outerHeight()) / 2) + 
-                                                $(window).scrollTop() + "px");
-				divToShow.css("left", (($(window).width() - divToShow.outerWidth()) / 2) + 
-                                                $(window).scrollLeft() + "px");
-				alert($(window).scrollTop());
-				// divToShow.innerHTML = serverData.html;
 				divToShow.html(serverData.html);
+				divToShow.css("top", (($(window).height() - divToShow.outerHeight()) / 2) + $(window).scrollTop() + "px");
+				divToShow.css("left", (($(window).width() - divToShow.outerWidth()) / 2) + $(window).scrollLeft() + "px");
 				divToShow.show();
 			}
 			else{
@@ -59,3 +33,50 @@ function sampleInsertShowDivAndCheckUser(resource){
 		)
 	;
 }
+
+function closeSampleInsertDiv(){
+	var divToShow = $('#sampleInterfaceDiv');
+	divToShow.html("");
+	divToShow.hide();
+}
+
+function sampleInsertOrRemove(resource, remove){
+	remove = remove || false;
+	var samples = new Array();
+	if(remove){
+		var list = document.getElementById('submittedSamples');
+		for(var i=list.options.length-1; i>=0; i--){
+			if(list.options[i].selected){
+				// samples will store the ids of the samples
+				samples[samples.length] = list.options[i].value;
+			}
+		}
+	}
+	else{
+		var sampleName = document.getElementById('sampleName').value;
+		if(sampleName == ''){
+			showMessage('Please insert a sample name');
+			return;
+		}
+		// samples will store the name of the sample, just one sample in this case
+		samples[samples.length] = sampleName;
+	}
+	
+	$.post(
+		"../agendo/sampleHandling.php"
+		,{action: 'sampleInsert', samples: samples, userLogin: userLogin, userPass: userPass, resource: resource, remove: remove}
+		,function(serverData){
+			if(serverData.success){
+				$('#submittedSamples').html(serverData.selectOptions);
+			}
+			showMessage(serverData.message, !serverData.success);
+		}
+		,"json")
+		.error(
+			function(errorData){
+				showMessage(errorData.responseText, true);
+			}
+		)
+	;
+}
+
--- agendo/sampleHandling.php
+++ agendo/sampleHandling.php
@@ -4,7 +4,7 @@
 	// ******************
 	// Gets or Posts here
 	try{
-	// This flag controls if the error data is sent to the client on page generation or sent to JS by reply
+		// This flag controls if the error data is sent to the client on page generation or sent to JS by reply
 		$dontEchoThis = isAjax();
 
 
@@ -14,15 +14,25 @@
 			if(!isset($_POST['resource'])){
 				throw new Exception('Resource not specified');
 			}
+			
+			$userId = getUserId($_POST['userLogin'], $_POST['userPass']);
+			// Need this comparison in case $userId = 0.... Thats php for you...
+			if($userId === false){
+				throw new Exception('Wrong username or password');
+			}
+		
+			if(!hasPermission($userId, $resource) && isResp($userId, $resource) !== false){ // dont allow resp to add samples? allow resp to add samples in someones name?
+				throw new Exception('User is not allowed to add samples');
+			}
 		
 			switch($_POST['action']){
 				case "sampleInsertHtml": // Regular user view
-					$html = sampleInsertHtml($_POST['resource'], $_POST['userLogin'], $_POST['userPass']);
+					$html = sampleInsertHtml($userId, $_POST['resource']);
 					$json->html = $html;
 				break;
-				case "insertSample":
+				case "sampleInsert":
 					if(isset($_POST['sampleName'])){
-						sampleInsert($_POST['resource'], $_POST['sampleName']);
+						$json->message = sampleInsert($_POST['sampleName'], $userId, $_POST['resource']);
 					}
 				break;
 			}
@@ -36,7 +46,7 @@
 		echo "<link href='../agendo/css/sample.css' rel='stylesheet' type='text/css' />";
 		echo "<script type='text/javascript' src='../agendo/js/sample.js'></script>";
 		
-		echo "<div id='sampleInterfaceDiv' style='display:none;background:white;width:340px;margin:auto;position:absolute;border:2px solid black;padding:20px;'>";
+		echo "<div id='sampleInterfaceDiv'>";
 		echo "</div>";
 	}
 	catch(Exception $e){
@@ -53,33 +63,16 @@
 
 	// *********
 	// Functions
-	function sampleInsertHtml($resource, $userLogin, $userPass){
-		if(isset($_SESSION['user_id'])){
-			$userId = $_SESSION['user_id'];
-		}
-		elseif(isset($userLogin) && isset($userPass)){
-			$sql = "select user_id from ".dbHelp::getSchemaName().".user where user_login = :0 and user_passwd = :1";
-			$prep = dbHelp::query($sql, array($userLogin, cryptPassword($userPass)));
-			if(dbHelp::numberOfRows($prep) > 0){
-				$row = dbHelp::fetchRowByIndex($prep);
-				$userId = $row[0];
-			}
-			else{
-				throw new Exception('Wrong user or password');
-			}
-		}
-		else{
-			throw new Exception('User not specified');
-		}
-		
-		if(!hasPermission($userId, $resource) && isResp($userId, $resource) !== false){
-			throw new Exception('User is not allowed to add samples');
-		}
-		
+	function sampleInsertHtml($userId, $resource){
 		$html = "";
 		$sql = "select resource_name from resource where resource_id = :0";
 		$prep = dbHelp::query($sql, array($resource));
 		$row = dbHelp::fetchRowByIndex($prep);
+		
+		$html .= "<a onclick='closeSampleInsertDiv();' onmouseover='this.style.cursor=\"pointer\"' style='position:absolute;top:0px;right:5px;color:#bb3322;font-size:16px;'>";
+			$html .= "x";
+		$html .= "</a>";
+		
 		$html .= "<label style='float:left;'>";
 			$html .= "New sample for ".$row[0].": ";
 		$html .= "</label>";
@@ -134,7 +127,26 @@
 		return $html;
 	}
 	
-	function sampleInsert(){
+	function sampleInsert($sampleName, $userId, $resource){
+		$sql = "insert into sample values(null, :0, :1, 1, :2)";
+		$prep = dbHelp::query($sql, array($sampleName, $userId, $resource));
+		return "Sample inserted";
+	}
+	
+	
+	// Returns a user id either from the session var or from the login and password text fields
+	function getUserId($userLogin, $userPass){
+		if(isset($_SESSION['user_id'])){
+			return $_SESSION['user_id'];
+		}
 		
+		$sql = "select user_id from ".dbHelp::getSchemaName().".user where user_login = :0 and user_passwd = :1";
+		$prep = dbHelp::query($sql, array($userLogin, cryptPassword($userPass)));
+		if(isset($userLogin) && isset($userPass) && dbHelp::numberOfRows($prep) > 0){
+			$row = dbHelp::fetchRowByIndex($prep);
+			return $row[0];
+		}
+		
+		return false;
 	}
 ?>
