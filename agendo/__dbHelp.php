4cd229515505cf9e7e11e120aa461c004b13b03b
PedroMP <zasdqw@gmail.com>
2013-03-15 18:55:09 +0000

    name change on a method

--- agendo/__dbHelp.php
+++ agendo/__dbHelp.php
@@ -98,7 +98,7 @@
 				// }
 				// self::errorLog("Full sql query is: '".trim(preg_replace("/\s\s+/", " ", $sql))."'".$argsText."\nError is: '".$e->getMessage()."'.\nError happened on: ".date("d/m/Y H:i:s")."\n");
 				$backTrace = debug_backtrace();
-				self::errorLog("Full sql query is: '".trim(preg_replace("/\s\s+/", " ", self::getRealQuery($sql, $argsArray)))."'".$argsText."\nError is: '".$e->getMessage()."'.\nError was generated on file: '".$backTrace[0]['file']."' on line: '".$backTrace[0]['line']."' \nError happened on: ".date("d/m/Y H:i:s")."\n");
+				self::errorLog("Full sql query is: '".trim(preg_replace("/\s\s+/", " ", self::get_real_query($sql, $argsArray)))."'".$argsText."\nError is: '".$e->getMessage()."'.\nError was generated on file: '".$backTrace[0]['file']."' on line: '".$backTrace[0]['line']."' \nError happened on: ".date("d/m/Y H:i:s")."\n");
 				throw new Exception("Database error, error logged.");
 			}
 			return $prepSql;
@@ -288,7 +288,7 @@
 			return $results;
 		}
 		
-		public static function getRealQuery($query, $query_array){
+		public static function get_real_query($query, $query_array){
 			if($query_array !== null){
 				foreach($query_array as $key => $value){
 					$query = str_replace(":".$key, $value, $query);
